73fbd6f552f690706ed2231e2647faab2cb260ab
g9h22 <g9h22@naver.com>
2023-04-28 14:56:00 +0900

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="fixed bottom-0 w-full h-[60] bg-gray-100 flex justify-center py-2 text-sm font-light">
-      Welcome to MEH world, designed by Sheep Lover
+      Welcome to the UMMEH world, designed by Sheep Lover
     </div>
   );
 }
